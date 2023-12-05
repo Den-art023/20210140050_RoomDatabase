d02a221ec71f0d5281cfd98ae53ca527243d62ba
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2023-12-06 03:34:13 +0700

    Membuat fun HomeScreen

--- app/src/main/java/com/den/roomsiswa/ui/theme/halaman/HalamanHome.kt
+++ app/src/main/java/com/den/roomsiswa/ui/theme/halaman/HalamanHome.kt
@@ -4,32 +4,83 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.den.roomsiswa.R
 import com.den.roomsiswa.data.Siswa
+import com.den.roomsiswa.model.HomeViewModel
+import com.den.roomsiswa.model.PenyediaViewModel
 import com.den.roomsiswa.navigasi.DestinasiNavigasi
+import com.den.roomsiswa.navigasi.SiswaTopAppBar
 
-object DestinasiHome: DestinasiNavigasi{
+object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
     override val titleRes = R.string.app_name
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection), topBar = {
+        SiswaTopAppBar(
+            title = stringResource(id = DestinasiHome.titleRes),
+            canNavigateBack = false,
+            scrollBehavior = scrollBehavior
+        )
+    }, floatingActionButton = {
+        FloatingActionButton(
+            onClick = navigateToItemEntry,
+            shape = MaterialTheme.shapes.medium,
+            modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+        ) {
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = stringResource(id = R.string.entry_siswa)
+            )
+        }
+    }
+    ) { innerPadding ->
+        val uiStateSiswa by viewModel.homeUiState.collectAsState()
+        BodyHome(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            itemSiswa = uiStateSiswa.listSiswa
+        )
+    }
+}
+
 @Composable
 fun BodyHome(
     modifier: Modifier,
