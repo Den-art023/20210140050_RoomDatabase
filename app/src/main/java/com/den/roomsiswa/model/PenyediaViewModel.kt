9d4c8a6545c7a2826528259d91d2ce3d88bcacb8
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2023-12-06 01:29:05 +0700

    Deklarasi inisialisasi untuk HomeViewModel dan EntryViewModel

--- app/src/main/java/com/den/roomsiswa/model/PenyediaViewModel.kt
+++ app/src/main/java/com/den/roomsiswa/model/PenyediaViewModel.kt
@@ -1,2 +1,24 @@
 package com.den.roomsiswa.model
 
+import android.text.Spannable.Factory
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.den.roomsiswa.ui.theme.AplikasiSiswa
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
