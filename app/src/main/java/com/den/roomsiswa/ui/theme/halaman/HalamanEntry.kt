8c45f237431befbe643bb709ca6832ea3e11df71
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2023-12-06 02:59:47 +0700

    Menyelesaikan fun EntrySiswaScreen

--- app/src/main/java/com/den/roomsiswa/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/den/roomsiswa/ui/theme/halaman/HalamanEntry.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -27,9 +29,11 @@ import com.den.roomsiswa.model.EntryViewModel
 import com.den.roomsiswa.model.PenyediaViewModel
 import com.den.roomsiswa.model.UIStateSiswa
 import com.den.roomsiswa.navigasi.DestinasiNavigasi
+import com.den.roomsiswa.navigasi.SiswaTopAppBar
+import kotlinx.coroutines.launch
 
-object DestinasiEntry: DestinasiNavigasi{
-    override val route= "item_entry"
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
     override val titleRes = R.string.entry_siswa
 }
 
@@ -39,14 +43,33 @@ fun EntrySiswaScreen(
     navigateBack: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
-){
+) {
     val coroutineScope = rememberCoroutineScope()
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     Scaffold(
         modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-        topBar = {}
-    ) {
-
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEntry.titleRes),
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior
+            )
+        }
+    ) { innerPadding ->
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.saveSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
     }
 }
 
