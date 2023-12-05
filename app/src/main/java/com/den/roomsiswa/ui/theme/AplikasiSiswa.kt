612bbe6ef8105df4e2e280dd1194121e4612b6c9
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2023-12-06 01:18:28 +0700

    Membuat fungsi untuk mengembalikan instance dari AplikasiSiswa

--- app/src/main/java/com/den/roomsiswa/ui/theme/AplikasiSiswa.kt
+++ app/src/main/java/com/den/roomsiswa/ui/theme/AplikasiSiswa.kt
@@ -1,4 +1,14 @@
 package com.den.roomsiswa.ui.theme
 
-class AplikasiSiswa {
+import android.app.Application
+import com.den.roomsiswa.repositori.ContainerApp
+import com.den.roomsiswa.repositori.ContainerDataApp
+
+class AplikasiSiswa : Application() {
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
 }
