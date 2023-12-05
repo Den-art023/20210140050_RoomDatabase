bf6df9878e75b6b9fa5af86d31421ac9a089e225
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2023-12-06 02:54:00 +0700

    Membuat fun SiswaTopAppBar di PetaNavigasi

--- app/src/main/java/com/den/roomsiswa/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/den/roomsiswa/ui/theme/halaman/HalamanEntry.kt
@@ -10,20 +10,44 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.den.roomsiswa.R
 import com.den.roomsiswa.model.DetailSiswa
+import com.den.roomsiswa.model.EntryViewModel
+import com.den.roomsiswa.model.PenyediaViewModel
 import com.den.roomsiswa.model.UIStateSiswa
 import com.den.roomsiswa.navigasi.DestinasiNavigasi
 
 object DestinasiEntry: DestinasiNavigasi{
     override val route= "item_entry"
     override val titleRes = R.string.entry_siswa
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {}
+    ) {
+
+    }
 }
 
 @Composable
