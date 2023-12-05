945ecfe629e42d9b53f19b568c808b17af0dc85e
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2023-12-06 00:46:42 +0700

    Membuat class HomeViewModel

--- app/src/main/java/com/den/roomsiswa/model/HomeViewModel.kt
+++ app/src/main/java/com/den/roomsiswa/model/HomeViewModel.kt
@@ -1,2 +1,6 @@
 package com.den.roomsiswa.model
 
+import androidx.lifecycle.ViewModel
+import com.den.roomsiswa.repositori.RepositoriSiswa
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa):ViewModel(){}
