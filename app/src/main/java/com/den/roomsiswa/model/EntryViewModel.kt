fe304ca5723f077a15ecd62d28c7d48b58333404
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2023-12-06 00:00:58 +0700

    Membuat class EntryViewModel di dalam file EntryViewModel.kt

--- app/src/main/java/com/den/roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/den/roomsiswa/model/EntryViewModel.kt
@@ -1,2 +1,8 @@
 package com.den.roomsiswa.model
 
+import androidx.lifecycle.ViewModel
+import com.den.roomsiswa.repositori.RepositoriSiswa
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+
+}
