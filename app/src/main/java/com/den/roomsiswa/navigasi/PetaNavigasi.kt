cb12999fae103d7b4b0d55795483165cf970f2b5
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2023-12-06 03:45:46 +0700

    Membuat fun HostNavigasi

--- app/src/main/java/com/den/roomsiswa/navigasi/PetaNavigasi.kt
+++ app/src/main/java/com/den/roomsiswa/navigasi/PetaNavigasi.kt
@@ -11,7 +11,15 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import com.den.roomsiswa.R
+import com.den.roomsiswa.ui.theme.halaman.DestinasiEntry
+import com.den.roomsiswa.ui.theme.halaman.DestinasiHome
+import com.den.roomsiswa.ui.theme.halaman.EntrySiswaScreen
+import com.den.roomsiswa.ui.theme.halaman.HomeScreen
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -35,4 +43,25 @@ fun SiswaTopAppBar(
                 }
             }
         })
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(navigateToItemEntry = { navController.navigate(DestinasiEntry.route) }
+            )
+        }
+        composable(DestinasiHome.route) {
+            EntrySiswaScreen(navigateBack = { navController.popBackStack() }
+            )
+        }
+    }
 }
